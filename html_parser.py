d56cbe3134d0ee663f580198553750e9ad7ce7ea
MP <pavlicic1marko@gmail.com>
2024-10-16 20:50:01 +0200

    adding html parse

---
+++ html_parser.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+
+def extract_body(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
