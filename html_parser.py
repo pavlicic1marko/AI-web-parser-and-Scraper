80b3c033c0f29b7ae42a0e3b0e0d8624519364d9
MP <pavlicic1marko@gmail.com>
2024-10-16 22:02:46 +0200

    parse html and remove scripts and styles...

--- html_parser.py
+++ html_parser.py
@@ -8,3 +8,15 @@ def extract_body(html_content):
         return str(body_content)
     return ""
 
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for scripts_or_style in soup(["scripts", "style"]):
+        scripts_or_style.extract()  # remove scripts and style from html
+
+    cleaned_content = soup.get_text(separator="\n")  # get text and separate it by new line
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content
+
