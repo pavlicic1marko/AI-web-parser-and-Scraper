0440360c729eb69640684790047ac9925062b737
MP <pavlicic1marko@gmail.com>
2025-12-28 16:42:56 +0200

    remove dpwnloadead 3 lines

--- venv/PIS/sve_delatnosti.py
+++ venv/PIS/sve_delatnosti.py
@@ -1,7 +1,4 @@
 dict1 = {
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=412000": 47,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=439900": 20,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432100": 19,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432200": 15,
 }
 #&page=2,&page=3
