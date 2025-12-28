ab4bdcfb651d910a5375026217751d5eddc004af
MP <pavlicic1marko@gmail.com>
2025-12-28 15:03:53 +0200

    remove dpwnloadead 14

--- venv/PIS/sve_delatnosti.py
+++ venv/PIS/sve_delatnosti.py
@@ -1,7 +1,6 @@
 dict1 = {
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=412000": 47,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=439900": 20,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=433900": 14,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432100": 19,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432200": 15,
 }
