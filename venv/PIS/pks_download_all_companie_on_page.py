5a6fa25fa76e18b5a5ab523ebdf5c2ea0246b1d0
MP <pavlicic1marko@gmail.com>
2025-12-28 12:32:18 +0200

    PI  improvement

--- venv/PIS/pks_download_all_companie_on_page.py
+++ venv/PIS/pks_download_all_companie_on_page.py
@@ -3,11 +3,9 @@ import PIS.scrap_single_company_data
 import PIS.write_data_to_file
 
 def dowload_comanoes_on_page(root_url):
-    pass
+    company_list = PIS.Scrap_all_companyes_on_page_of_PKS.get_all_company_links(root_url)
+    for company_url in company_list:
+        company_data = PIS.scrap_single_company_data.scrape_company(company_url)
+        PIS.write_data_to_file.write_data(company_data)
 
-root_url = "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=431200"
 
-company_list = PIS.Scrap_all_companyes_on_page_of_PKS.get_all_company_links(root_url)
-for company_url in company_list:
-    company_data = PIS.scrap_single_company_data.scrape_company(company_url)
-    PIS.write_data_to_file.write_data(company_data)
--- venv/PIS/sve_delatnosti.py
+++ venv/PIS/sve_delatnosti.py
@@ -6,10 +6,7 @@ dict1 = {
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=429100": 7,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=421300": 2,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=429900": 2,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=421100": 10,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=412000": 47,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=421200": 1,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=433100": 1,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432900": 7,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=439900": 20,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=433900": 14,
@@ -18,5 +15,5 @@ dict1 = {
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=432200": 15,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=411000": 5,
     "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=431100": 3,
-    "https://privredni-imenik.com/pretraga?keyword=&cities_id=0&category_id=8&sub_category_id=433200": 3,
 }
+#&page=2,&page=3
